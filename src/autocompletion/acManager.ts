1623c694a2efb27727efd26eb514184122fa0bed
Babis@Cs-MacBook-Pro <c.kyfonidis@kcl.ac.uk>
2021-02-09 12:41:40 +0000

    flag missing added

--- src/autocompletion/acManager.ts
+++ src/autocompletion/acManager.ts
@@ -184,7 +184,7 @@ export function getCandidatesForAC(slotCode: string, frameId: number, acSpanId: 
     */
 
     const parser = new Parser();
-    const userCode = parser.getCodeWithoutErrors(frameId);
+    const userCode = parser.getCodeWithoutErrors(frameId,true);
     prepareBrythonCode(userCode, contextAC, acSpanId, documentationSpanId, false);
     return {tokenAC: tokenAC , contextAC: contextAC, showAC: true};
 }
