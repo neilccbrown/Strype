4f2c654fbc696e68c9d3369b689026952c081f56
neil <neil@twistedsquare.com>
2022-09-13 18:52:14 +0100

    Added support for testing with jest and vue-test-utils v1, and added a first simple proof-of-concept unit test.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,20 @@
 module.exports = {
     root: true,
+
     env: {
         node: true,
     },
+
     extends: [
         "plugin:vue/essential",
         "eslint:recommended",
         "@vue/typescript/recommended",
     ],
+
     parserOptions: {
         ecmaVersion: 2020,
     },
+
     rules: {
         quotes: ["error", "double"],
         curly: "error",
@@ -32,4 +36,16 @@ module.exports = {
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     },
+
+    overrides: [
+        {
+            files: [
+                "**/__tests__/*.{j,t}s?(x)",
+                "**/tests/unit/**/*.spec.{j,t}s?(x)",
+            ],
+            env: {
+                jest: true,
+            },
+        },
+    ],
 };
