a933a1f77595f674f20aef36a2ca04d708a36120
neil <neil@twistedsquare.com>
2022-09-21 14:43:25 +0100

    Reverted the recent change to i18n that is no longer needed now that we are using mochapack for testing.

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,12 +4,20 @@ import VueI18n, { LocaleMessages } from "vue-i18n"
 Vue.use(VueI18n)
 
 function loadLocaleMessages (): LocaleMessages {
-    return {
-        "el": require("./localisation/el.json"),
-        "en": require("./localisation/en.json"),
-        "fr": require("./localisation/fr.json"),
-        "ja": require("./localisation/ja.json"),
-    };
+    const locales = require.context(
+        "./localisation",
+        true,
+        /[A-Za-z0-9-_,\s]+\.json$/i
+    )
+    const messages: LocaleMessages = {}
+    locales.keys().forEach((key) => {
+        const matched = key.match(/([A-Za-z0-9-_]+)\./i)
+        if (matched && matched.length > 1) {
+            const locale = matched[1]
+            messages[locale] = locales(key)
+        }
+    })
+    return messages
 }
 
 export default new VueI18n({
