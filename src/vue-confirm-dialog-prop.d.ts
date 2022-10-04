421e11e55db0f137ed3efa4c4e3e0546258e96ae
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2022-10-04 10:40:35 +0100

    Restored a seemingly-unused import that was not unused.

--- src/vue-confirm-dialog-prop.d.ts
+++ src/vue-confirm-dialog-prop.d.ts
@@ -1,3 +1,5 @@
+// This import appears unused but breaks the build if you remove it:
+import Vue from "vue"; // eslint-disable-line @typescript-eslint/no-unused-vars
 declare module "vue/types/vue" {
   interface VueConstructor {
     $confirm: any;
