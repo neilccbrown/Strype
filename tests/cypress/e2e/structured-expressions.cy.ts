4a283171337bdd2aeee8b50d6b47e48dbb23e51b
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2022-12-08 15:21:33 +0000

    Added a couple of attributes that allow testing of the cursor position.  Thus re-enabled the cursor position tests (which all pass, once I fix up the new tests which I hadn't adjusted properly).

--- tests/cypress/e2e/structured-expressions.cy.ts
+++ tests/cypress/e2e/structured-expressions.cy.ts
@@ -1,26 +1,38 @@
 function assertState(expectedState : string) : void {
-    cy.get("#FrameContainer_-3 .frame-header").first().within((h) => cy.get(".labelSlot-input,.frameColouredLabel").should((parts) => {
-        let s = "";
-        if (!parts) {
-            // Try to debug an occasional seemingly impossible failure:
-            cy.task("log", "Parts is null which I'm sure shouldn't happen, came from frame: " + h);
-        }
-        for (let i = 0; i < parts.length; i++) {
-            const p : any = parts[i];
-
-            const text = p.value || p.textContent || "";
-            
-            if (p.getAttribute("contenteditable") == "true" && !p.classList.contains("string-slot")) {
-                s += "{" + text + "}";
+    withSelection((info) => {
+        cy.get("#FrameContainer_-3 .frame-header").first().within((h) => cy.get(".labelSlot-input,.frameColouredLabel").then((parts) => {
+            let s = "";
+            if (!parts) {
+                // Try to debug an occasional seemingly impossible failure:
+                cy.task("log", "Parts is null which I'm sure shouldn't happen, came from frame: " + h);
             }
-            else {
+            for (let i = 0; i < parts.length; i++) {
+                const p : any = parts[i];
+    
+                let text = p.value || p.textContent || "";
+                
+                if (p.getAttribute("contenteditable") == "true") {
+                    // If we're the focused slot, put a dollar sign in to indicate the current cursor position:
+                    if (info.id === p.getAttribute("id") && info.cursorPos >= 0) {
+                        text = text.substring(0, info.cursorPos) + "$" + text.substring(info.cursorPos);
+                    }
+                    if (!p.classList.contains("string-slot")) {
+                        text = "{" + text + "}";
+                    }
+                }
                 s += text;
             }
-        }
-        // For now, we ignore cursor position (indicated by $):
-        // Also, there is no correspondence for _ (indicating a null operator) in the Strype interface so just ignore that:
-        expect(s).to.equal(expectedState.replace("$", "").replaceAll("_", ""));
-    }));
+            // There is no correspondence for _ (indicating a null operator) in the Strype interface so just ignore that:
+            expect(s).to.equal(expectedState.replaceAll("_", ""));
+        }));
+    });
+}
+
+function withSelection(inner : (arg0: { id: string, cursorPos : number }) => void) : void {
+    cy.get("#editor").then((eds) => {
+        const ed = eds.get()[0];
+        inner({id : ed.getAttribute("data-slot-focus-id") || "", cursorPos : parseInt(ed.getAttribute("data-slot-cursor") || "-2")});
+    });
 }
 
 function testInsert(insertion : string, result : string) : void {
@@ -32,7 +44,7 @@ function testInsert(insertion : string, result : string) : void {
         cy.get("body").type(" " + insertion);
         assertState(result);
 
-        // TODO test caret position?
+        // TODO test caret position mapping?
         // TODO test splitting the insert like in Java
     });    
 }
@@ -87,9 +99,9 @@ describe("Stride TestExpressionSlot.testOperators()", () => {
     //testInsert("false!=!!!false", "{false}!={}!{}!{}!{false$}");
     // Must surround these in a method call because otherwise the = gets converted
     // to an assignment:
-    testInsert("foo(5==-6)", "{foo}({5}=={-6$}){}");
-    testInsert("foo(5==--6)", "{foo}({5}=={}-{-6$}){}");
-    testInsert("foo(5==----6)", "{foo}({5}=={}-{}-{}-{-6$}){}");
+    testInsert("foo(5==-6)", "{foo}({5}=={-6}){$}");
+    testInsert("foo(5==--6)", "{foo}({5}=={}-{-6}){$}");
+    testInsert("foo(5==----6)", "{foo}({5}=={}-{}-{}-{-6}){$}");
     testInsert("a.b", "{a}.{b$}");
     //testInsert("a..b", "{a}..{b$}");
     testInsert("y-1", "{y}-{1$}");
