e58aab83a3ed9a99af7a6086512a2d3c3b0d55f7
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2023-11-17 08:33:37 +0000

    Removed the Babel plugins, which I think may not be necessary any more.

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,3 @@
 module.exports = {
     presets: ["@vue/cli-plugin-babel/preset"],
-    // From https://github.com/vuejs/vue-cli/issues/6833
-    // Needed to handle ?. operator when running tests with mochapack:
-    plugins: [
-        "@babel/plugin-transform-nullish-coalescing-operator",
-        "@babel/plugin-transform-optional-chaining",
-    ],
 };
