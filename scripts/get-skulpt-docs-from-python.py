eaf1e9df595a7c1e95b6e6dd1e2af24491e795c8
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2023-12-05 11:11:46 +0000

    Stopped showing type signatures in our documentation for pure Python items.  Although technically useful, I think it's too confusing for beginners.

--- scripts/get-skulpt-docs-from-python.py
+++ scripts/get-skulpt-docs-from-python.py
@@ -5,6 +5,7 @@
 import importlib
 import inspect
 import json
+import re
 import sys
 
 skulptAPI = json.load(sys.stdin)
@@ -19,7 +20,13 @@ for mod in skulptAPI:
         if not item['acResult'].startswith("_"):
             try:
                 doc = inspect.getdoc(getattr(imp_mod, item['acResult']))
-                item['documentation'] = doc
+                # Some functions now have their type signature first, which we will omit by removing up
+                # to the first \n\n that follows such signature(s):
+                if re.compile("^[A-Za-z0-9]+\\(").match(doc):
+                    doubleNL = doc.find("\n\n")
+                    if doubleNL > 0:
+                        doc = doc[doubleNL:]
+                item['documentation'] = doc.strip()
             except:
                 pass
 
