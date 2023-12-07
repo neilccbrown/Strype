647747271422db5789441e1f22541c0364ab4e54
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2023-12-07 13:48:17 +0000

    Reworked the process-skulpt-api script to avoid a race hazard involving the order of promises and when we fetch the global variable result from Skulpt.

--- scripts/process-skulpt-api.ts
+++ scripts/process-skulpt-api.ts
@@ -17,50 +17,54 @@ import "../public/js/skulpt.min";
 import "../public/js/skulpt-stdlib";
 import { pythonBuiltins } from "../src/autocompletion/pythonBuiltins";
 import {configureSkulptForAutoComplete, getPythonCodeForNamesInContext} from "../src/autocompletion/ac-skulpt";
-import { AcResultType } from "../src/types/ac-types";
-
+import {AcResultsWithModule} from "../src/types/ac-types";
 
 declare const Sk: any;
 declare const window: any;
 
-const promises : Promise<{[module: string] : AcResultType[]}>[] = [];
-for (const key in pythonBuiltins) {
-    if (pythonBuiltins[key].type === "module") {
-        // Ask Skulpt about the module's contents:
-        const codeToRun = getPythonCodeForNamesInContext("import " + key + "\n", key);
-        configureSkulptForAutoComplete();
-        const myPromise = Sk.misceval.asyncToPromise(function() {
-            return Sk.importMainWithBody("<stdin>", false, codeToRun, true);
-        }, {});
-        // Show error in JS console if error happens
-        promises.push(myPromise.then(() => {
-            return Promise.resolve({[key] : (Sk.ffi.remapToJs(Sk.globals["acs"]) as string[]).map((s) => ({acResult: s, documentation: "", type: "unknown", version: 0}))});
-        },
-        (err: any) => {
-            console.log("Error running autocomplete code: " + err + "Code was:\n" + codeToRun);
-        }));
-    }
-}
-Promise.all(promises).then((results) => {
-    const moduleContents: Record<string, AcResultType[]> = {};
-    moduleContents[""] = [];
-    for (let i = 0; i < results.length; i++) {
-        for (const resultKey in results[i]) {
-            moduleContents[resultKey] = results[i][resultKey];
-        }
-    }
-    // Also add Skulpt's builtin functions to the default module:
-    for (const func of Object.keys(Sk.builtin)) {
-        if (!func.includes("$")) {
-            moduleContents[""].push({acResult: func, documentation: "", version: 0, type: "function"});
-        }
+const modules : string[] = Object.keys(pythonBuiltins).filter((k) => pythonBuiltins[k].type === "module");
+
+// The challenge with Skulpt is that everything is in a Promise, but we have to be careful that we pull out
+// the global acs variable before running the next Skulpt.  We can't await the Promise because the "browser-run"
+// environment we are in does not support top-level await.  So instead, we just make an absolutely huge
+// promise chain that adds each item to the list until there is nothing left to process, then we output them
+// all and tell the browser to exit:
+
+function getModuleMembersOneByOne(next : number, soFar : AcResultsWithModule, atEnd: () => void) {
+    if (next >= modules.length) {
+        // Done everything, so execute last bit instead:
+        atEnd();
+        return;
     }
     
+    // Ask Skulpt about the module's contents:
+    const codeToRun = getPythonCodeForNamesInContext("import " + modules[next] + "\n", modules[next]);
+    configureSkulptForAutoComplete();
+    const myPromise = Sk.misceval.asyncToPromise(function() {
+        return Sk.importMainWithBody("<stdin>", false, codeToRun, true);
+    }, {});
+    // Show error in JS console if error happens
+    myPromise.then(() => {
+        soFar[modules[next]] = (Sk.ffi.remapToJs(Sk.globals["acs"]) as string[]).map((s) => ({acResult: s, documentation: "", type: "unknown", version: 0}));
+        getModuleMembersOneByOne(next + 1, soFar, atEnd);
+    },
+    (err: any) => {
+        console.log("Error running autocomplete code: " + err + "Code was:\n" + codeToRun);
+    });
+}
+
+const allContent : AcResultsWithModule = {"": []};
+// Add Skulpt's builtin functions to the default module:
+for (const func of Object.keys(Sk.builtin)) {
+    if (!func.includes("$")) {
+        allContent[""].push({acResult: func, documentation: "", version: 0, type: "function"});
+    }
+}
+getModuleMembersOneByOne(0, allContent, () => {    
     // Outputting the results to console actually goes to stdout, which the surrounding
     // task redirects to the API file:
-    console.log(JSON.stringify(moduleContents, null, 4));
+    console.log(JSON.stringify(allContent, null, 4));
     // This tells browser-run to exit, without it the browser stays open and the process never terminates.
     // However, if we do it immediately it seems the console is not flushed to stdout, so we put it on a timer:
     setTimeout(() => window.close(), 2000);
-    
 });
