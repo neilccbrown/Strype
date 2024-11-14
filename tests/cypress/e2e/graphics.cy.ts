b3747bbe83ebee59a1e2d02ea782819bdaf157a5
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2024-11-14 18:08:08 +0000

    Added two further tests; one for downloading an image after editing it, and one for the rate limiting of the downloads.

--- tests/cypress/e2e/graphics.cy.ts
+++ tests/cypress/e2e/graphics.cy.ts
@@ -153,7 +153,7 @@ function checkImageViaDownload(functions: string, main: string, downloadStem: st
     });
 }
 
-function enterAndExecuteCode(functions: string, main: string) {
+function enterAndExecuteCode(functions: string, main: string, timeToWaitMillis = 5000) {
     cy.get("body").type("{uparrow}{uparrow}");
     (cy.get("body") as any).paste("from strype.graphics import *\nfrom time import sleep\n");
     cy.wait(2000);
@@ -167,7 +167,7 @@ function enterAndExecuteCode(functions: string, main: string) {
     cy.get("#runButton").contains("Run");
     cy.get("#runButton").click();
     // Wait for it to finish:
-    cy.wait(5000);
+    cy.wait(timeToWaitMillis);
     // Assert it has finished, by looking at the run button:
     cy.get("#runButton").contains("Run");
 }
@@ -261,5 +261,63 @@ describe("Image download", () => {
         checkImageViaDownload("", `
             cat = Actor('cat-test.jpg')
             cat.edit_image().download()`, "strype-image", "download-plain-cat");
+    });
+
+    it("Downloads cat with coloured cross", () => {
+        checkImageViaDownload("", `
+            cat = load_image('cat-test.jpg')
+            cat.set_stroke("red")
+            cat.line(0, 0, cat.get_width(), cat.get_height())
+            cat.set_stroke("blue")
+            cat.line(cat.get_width(), 0, 0, cat.get_height())
+            cat.download("cat-crossed")`, "cat-crossed", "download-cat-crossed", ImageComparison.WRITE_NEW_EXPECTED_DO_NOT_COMMIT_USE_OF_THIS);
+    });
+    
+    it("Rate limits downloads to one every two seconds", () => {
+        // To test this, we write code which tries to download a lot of images very quickly
+        // but the timestamps on them should always be at least one second apart:
+        focusEditorPasteAndClear();
+        // The timestamp should be between before and after:
+        const before = Date.now();
+        enterAndExecuteCode("", `
+            # Make small images to make saving very fast:
+            a = EditableImage(10, 10)
+            b = EditableImage(10, 10)
+            c = EditableImage(10, 10)
+            a.download()
+            a.download()
+            a.download()
+            b.download()
+            c.download()
+            a.download()
+            b.download()
+            c.download()
+            c.download()
+        `, 9 * 2000 + 5000);
+        (cy.task("downloads") as Cypress.Chainable<string[]>).then((allFiles: string[]) => {
+            // We need the after to be in here because all the prior calls just queue things up like promises,
+            // the code will not actually have executed.  But inside a .then(), the code will have
+            // executed:
+            const after = Date.now();
+            expect(allFiles.length).to.be.at.least(9);
+            // Must have taken at least this long given it was rate limited:
+            expect(after - before).to.be.above(9 * 2000);
+            
+            // If there is a file for a particular timestamp, there should not be another one 
+            // one second later:
+            let inRangeCount = 0;
+            // Search for the filenames one second at a time:
+            for (let d = before; d <= after; d += 1000) {
+                const filename = `strype-image_${formatDate(d)}.png`;
+                if (allFiles.includes(filename)) {
+                    inRangeCount += 1;
+                    // Shouldn't contain an image saved one second later:
+                    expect(allFiles).not.contains(`strype-image_${formatDate(d + 1000)}.png`);
+                }
+            }
+            
+            // Check they were all found in the time range:
+            expect(inRangeCount).to.equal(9);
+        });
     });
 });
