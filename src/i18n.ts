415a2cf83d485a18010e3af4c50a937e341cb533
Pierre Weill-Tessier <pierre.weill-tessier@kcl.ac.uk>
2024-11-25 12:17:45 +0000

    Support for new localisation files organisation (sub folder per locale, split of JSON locale files)

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,17 +4,24 @@ import VueI18n, { LocaleMessages } from "vue-i18n";
 Vue.use(VueI18n);
 
 function loadLocaleMessages (): LocaleMessages {
+    // Strype locale files are store in locale folders (2 letter coded name, e.g. "en")
+    // that are living in ./localisation. A locale folder may contain more than 1 file
+    // as we split the json files for easier work on the translations.
     const locales = require.context(
         "./localisation",
         true,
-        /[A-Za-z0-9-_,\s]+\.json$/i
+        /[A-Za-z]{2}_.*\.json$/i
     );
     const messages: LocaleMessages = {};
+    // We restrict the regex here against the file naming we mentioned above.
+    // Since our locale files are split, we need to "combine" the json KPV in one, 
+    // which can be achieved by appending the found KVP for a same locale
+    // (see https://stackoverflow.com/questions/60628853/how-to-use-multiple-files-per-language-with-vue-i18n-in-vue)        
     locales.keys().forEach((key) => {
-        const matched = key.match(/([A-Za-z0-9-_]+)\./i);
+        const matched = key.match(/\.\/[A-Za-z]{2}\/([A-Za-z]{2})_/i);
         if (matched && matched.length > 1) {
             const locale = matched[1];
-            messages[locale] = locales(key);
+            messages[locale] = {...messages[locale], ...locales(key)};
         }
     });
     return messages;
