e983fdffdbe3ec8ba94e2e909e618ae639697edd
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2024-11-29 14:11:16 +0000

    Removed a test which seems to be flaky based on whether a particular Cypress oddity manifests or not.

--- tests/cypress/e2e/autocomplete.cy.ts
+++ tests/cypress/e2e/autocomplete.cy.ts
@@ -1057,27 +1057,6 @@ describe("Parameter prompts", () => {
             }
             
         });
-        it("Shows prompts after using AC for " + func.displayName, () => {
-            focusEditorAC();
-            if (func.keyboardTypingToImport) {
-                cy.get("body").type(func.keyboardTypingToImport);
-            }
-            cy.get("body").type(" " + func.funcName);
-            cy.get("body").type("{ctrl} ");
-            cy.wait(1000);
-            // There is a bug which only seems to happen in cypress where the selection
-            // pings back to the start of the slot; I don't see this in a real browser
-            // We compensate by moving the cursor back to the end with right arrow:
-            cy.get("body").type("{rightarrow}".repeat(func.funcName.includes(".") ? func.funcName.length - func.funcName.lastIndexOf(".") - 1 : func.funcName.length));
-            if (!func.funcName.includes(".")) {
-                withAC((acIDSel) => {
-                    cy.get(acIDSel).should("be.visible");
-                    checkExactlyOneItem(acIDSel, func.acSection, func.acName + "(" + func.params.join(", ") + ")");
-                });
-            }
-            cy.get("body").type("{enter}");
-            withFrameId((frameId) => assertState(frameId, func.funcName + "($)", func.funcName + "(" + func.params.join(", ") + ")"));
-        });
 
         it("Shows prompts in nested function (part 1) " + func.displayName, () => {
             focusEditorAC();
