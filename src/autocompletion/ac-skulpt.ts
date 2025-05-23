6f25c95e2e7b34ef20ce20c35828af712c88acf9
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2025-05-23 17:18:41 +0100

    Removed all the mediacomp files, ready for moving it to a third-party library.

--- src/autocompletion/ac-skulpt.ts
+++ src/autocompletion/ac-skulpt.ts
@@ -9,7 +9,6 @@ export const OUR_PUBLIC_LIBRARY_FILES : string[] = [
     "strype/__init__.py",
     "strype/strype_graphics_internal.js", "strype/graphics.py",
     "strype/strype_sound_internal.js", "strype/sound.py",
-    "strype/mediacomp.py",
     "strype/strype_graphics_input_internal.js",
 ];
 export const OUR_PUBLIC_LIBRARY_MODULES = OUR_PUBLIC_LIBRARY_FILES.map((f) => f.substring(0, f.lastIndexOf(".")).replace("/", ".")).filter((f) => !f.includes("internal") && !f.includes("__init__"));
