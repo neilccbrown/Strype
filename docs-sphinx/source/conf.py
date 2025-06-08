946bf74565f3a8bcdd3db5c2f330e6b84292e107
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2025-06-08 10:34:14 +0100

    Moved some of the sphinx config into a shared file intended for re-use by third-party libraries.

--- docs-sphinx/source/conf.py
+++ docs-sphinx/source/conf.py
@@ -4,8 +4,11 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 import os
 import sys
+sys.path.insert(0, os.path.abspath('.'))
+import strype_sphinx_conf_defaults
 sys.path.insert(0, os.path.abspath('../../public/public_libraries'))
 sys.path.insert(0, os.path.abspath('./_dummy'))
+
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -17,22 +20,7 @@ author = 'K-PET Group, King\'s College London'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
-
 templates_path = ['_templates']
 exclude_patterns = []
 
-autoclass_content = 'both'
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'classic'
-html_static_path = ['_static']
-html_css_files = [
-    'custom.css',
-]
-html_theme_options = {
-    'stickysidebar': True,
-}
+strype_sphinx_conf_defaults.set_defaults(globals())
