1eee96bcf31a892e5ddd5492e6bd3461d3588f58
Pierre Weill-Tessier <pierre.weill-tessier@kcl.ac.uk>
2025-10-28 14:04:40 +0000

    For string literal slots, leave home/end be handled by the browser #607

--- src/components/LabelSlot.vue
+++ src/components/LabelSlot.vue
@@ -1129,11 +1129,11 @@ export default Vue.extend({
         },
 
         handleFastUDNavKeys(event: KeyboardEvent){
-            // If we are in a comment, we let the browser handling the key events.
+            // If we are in a comment or in a string slot, we let the browser handling the key events.
             // Otherwise, the following rules apply:
             // Home/End move the text cursor to the start/end of the label slot
             // PageUp/PageDown: do nothing
-            if(this.frameType == AllFrameTypesIdentifier.comment){
+            if(this.frameType == AllFrameTypesIdentifier.comment || this.slotType == SlotType.string){
                 return;
             }
             else if(event.key == "Home" || event.key == "End"){
