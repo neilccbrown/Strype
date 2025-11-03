03de1c4160e6b93413a51c485491734bd3e1ed51
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2025-11-03 17:22:46 +0000

    Added tests for what should happen when repeatedly pressing Ctrl-A in classes.

--- tests/playwright/e2e/frame-selection-manipulation.spec.ts
+++ tests/playwright/e2e/frame-selection-manipulation.spec.ts
@@ -120,7 +120,7 @@ def first ( ) :
 `);
     });
 
-    test("Test Ctrl-A thrice in a function selects all functions", async ({page}) => {
+    test("Test Ctrl-A three times in a function selects all functions", async ({page}) => {
         // So the paste will end up cutting all functions and pasting them back, so it looks like no change:
         await testBeforeAfterPaste(page,`def first():
    print('0') 
@@ -139,6 +139,190 @@ def second ( ) :
     if x>0  :
         print('B') 
     print('C') 
+#(=> Section:Main
+#(=> Section:End
+`);
+    });
+    
+    const rep = <T>(n: number, x: T) => Array(n).fill(x);
+    
+    test("Test Ctrl-A once in a class function selects whole function content", async ({page}) => {
+        // Transfers content from src to dest functions: 
+        await testBeforeAfterPaste(page,`class Dest:
+   dest_member = 0
+   def dest(self):
+       print('0')
+class Src: 
+    src_member_1 = 0
+    def src(self):
+        print('A')
+        if x > 0:
+            print('B')
+        print('C')
+    def src2():
+        print('D')
+`, /* We get below print('B'): */ ["End", ...rep(8, "ArrowUp"), ...rep(1, "Control+a")], "cut", ["Home", "ArrowDown", "ArrowDown", "ArrowDown"],`#(=> Strype:1:std
+#(=> Section:Imports
+#(=> Section:Definitions
+class Dest  :
+    dest_member  = 0 
+    def dest (self, ) :
+        print('A') 
+        if x>0  :
+            print('B') 
+        print('C') 
+        print('0') 
+class Src  :
+    src_member_1  = 0 
+    def src (self, ) :
+        pass
+    def src2 (self, ) :
+        print('D') 
+#(=> Section:Main
+#(=> Section:End
+`);
+    });
+
+    test("Test Ctrl-A twice in a class function selects whole function", async ({page}) => {
+        // Transfers src function to Dest class: 
+        await testBeforeAfterPaste(page,`class Dest:
+   dest_member = 0
+   def dest(self):
+       print('0')
+class Src: 
+    src_member_1 = 0
+    def src(self):
+        print('A')
+        if x > 0:
+            print('B')
+        print('C')
+    def src2():
+        print('D')
+`, /* We get below print('B'): */ ["End", ...rep(8, "ArrowUp"), ...rep(2, "Control+a")], "cut", ["Home", "ArrowDown", "ArrowDown"],`#(=> Strype:1:std
+#(=> Section:Imports
+#(=> Section:Definitions
+class Dest  :
+    dest_member  = 0 
+    def src (self, ) :
+        print('A') 
+        if x>0  :
+            print('B') 
+        print('C') 
+    def dest (self, ) :
+        print('0') 
+class Src  :
+    src_member_1  = 0 
+    def src2 (self, ) :
+        print('D') 
+#(=> Section:Main
+#(=> Section:End
+`);
+    });
+
+    test("Test Ctrl-A three times in a class function selects whole class content", async ({page}) => {
+        // Transfers all class content from Src to Dest: 
+        await testBeforeAfterPaste(page,`class Dest:
+   dest_member = 0 
+   def dest(self):
+       print('0') 
+class Src: 
+    src_member_1 = 0 
+    def src(self):
+        print('A')
+        if x > 0:
+            print('B')
+        print('C') 
+    def src2():
+        print('D') 
+`, /* We get below print('B'): */ ["End", ...rep(8, "ArrowUp"), ...rep(3, "Control+a")], "cut", ["Home", "ArrowDown", "ArrowDown"],`#(=> Strype:1:std
+#(=> Section:Imports
+#(=> Section:Definitions
+class Dest  :
+    dest_member  = 0 
+    src_member_1  = 0 
+    def src (self, ) :
+        print('A') 
+        if x>0  :
+            print('B') 
+        print('C') 
+    def src2 (self, ) :
+        print('D') 
+    def dest (self, ) :
+        print('0') 
+class Src  :
+    pass
+#(=> Section:Main
+#(=> Section:End
+`);
+    });
+
+    test("Test Ctrl-A four times in a class function selects whole class", async ({page}) => {
+        // Moves Src class above dest: 
+        await testBeforeAfterPaste(page,`class Dest:
+   dest_member = 0
+   def dest(self):
+       print('0')
+class Src: 
+    src_member_1 = 0 
+    def src(self):
+        print('A')
+        if x > 0:
+            print('B')
+        print('C')
+    def src2():
+        print('D')
+`, /* We get below print('B'): */ ["End", ...rep(8, "ArrowUp"), ...rep(4, "Control+a")], "cut", ["Home"],`#(=> Strype:1:std
+#(=> Section:Imports
+#(=> Section:Definitions
+class Src  :
+    src_member_1  = 0 
+    def src (self, ) :
+        print('A') 
+        if x>0  :
+            print('B') 
+        print('C') 
+    def src2 (self, ) :
+        print('D') 
+class Dest  :
+    dest_member  = 0 
+    def dest (self, ) :
+        print('0') 
+#(=> Section:Main
+#(=> Section:End
+`);
+    });
+
+    test("Test Ctrl-A five times in a class function selects all definitions", async ({page}) => {
+        // Basically does nothing because everything ends where it began: 
+        await testBeforeAfterPaste(page,`class Dest:
+   dest_member = 0
+   def dest(self):
+       print('0')
+class Src: 
+    src_member_1 = 0
+    def src(self):
+        print('A')
+        if x > 0:
+            print('B')
+        print('C')
+    def src2():
+        print('D')
+`, /* We get below print('B'): */ ["End", ...rep(8, "ArrowUp"), ...rep(5, "Control+a")], "cut", ["Home"],`#(=> Strype:1:std
+#(=> Section:Imports
+#(=> Section:Definitions
+class Dest  :
+    dest_member  = 0 
+    def dest (self, ) :
+        print('0') 
+class Src  :
+    src_member_1  = 0 
+    def src (self, ) :
+        print('A') 
+        if x>0  :
+            print('B') 
+        print('C') 
+    def src2 (self, ) :
+        print('D') 
 #(=> Section:Main
 #(=> Section:End
 `);
