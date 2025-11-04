af3ca2739e1848b175196fba6e9610587cbbe5ae
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2025-11-04 09:35:54 +0000

    Added a couple more select all tests.

--- tests/playwright/e2e/frame-selection-manipulation.spec.ts
+++ tests/playwright/e2e/frame-selection-manipulation.spec.ts
@@ -145,13 +145,11 @@ def second ( ) :
     });
     
     const rep = <T>(n: number, x: T) => Array(n).fill(x);
-    
-    test("Test Ctrl-A once in a class function selects whole function content", async ({page}) => {
-        // Transfers content from src to dest functions: 
-        await testBeforeAfterPaste(page,`class Dest:
-   dest_member = 0
-   def dest(self):
-       print('0')
+
+    const classTestInput = `class Dest:
+    dest_member = 0
+    def dest(self):
+        print('0')
 class Src: 
     src_member_1 = 0
     def src(self):
@@ -161,7 +159,10 @@ class Src:
         print('C')
     def src2():
         print('D')
-`, /* We get below print('B'): */ ["End", ...rep(8, "ArrowUp"), ...rep(1, "Control+a")], "cut", ["Home", "ArrowDown", "ArrowDown", "ArrowDown"],`#(=> Strype:1:std
+`;
+    test("Test Ctrl-A once in a class function selects whole function content", async ({page}) => {
+        // Transfers content from src to dest functions: 
+        await testBeforeAfterPaste(page,classTestInput, /* We get below print('B'): */ ["End", ...rep(8, "ArrowUp"), ...rep(1, "Control+a")], "cut", ["Home", "ArrowDown", "ArrowDown", "ArrowDown"],`#(=> Strype:1:std
 #(=> Section:Imports
 #(=> Section:Definitions
 class Dest  :
@@ -185,20 +186,7 @@ class Src  :
 
     test("Test Ctrl-A twice in a class function selects whole function", async ({page}) => {
         // Transfers src function to Dest class: 
-        await testBeforeAfterPaste(page,`class Dest:
-   dest_member = 0
-   def dest(self):
-       print('0')
-class Src: 
-    src_member_1 = 0
-    def src(self):
-        print('A')
-        if x > 0:
-            print('B')
-        print('C')
-    def src2():
-        print('D')
-`, /* We get below print('B'): */ ["End", ...rep(8, "ArrowUp"), ...rep(2, "Control+a")], "cut", ["Home", "ArrowDown", "ArrowDown"],`#(=> Strype:1:std
+        await testBeforeAfterPaste(page, classTestInput, /* We get below print('B'): */ ["End", ...rep(8, "ArrowUp"), ...rep(2, "Control+a")], "cut", ["Home", "ArrowDown", "ArrowDown"],`#(=> Strype:1:std
 #(=> Section:Imports
 #(=> Section:Definitions
 class Dest  :
@@ -221,20 +209,7 @@ class Src  :
 
     test("Test Ctrl-A three times in a class function selects whole class content", async ({page}) => {
         // Transfers all class content from Src to Dest: 
-        await testBeforeAfterPaste(page,`class Dest:
-   dest_member = 0 
-   def dest(self):
-       print('0') 
-class Src: 
-    src_member_1 = 0 
-    def src(self):
-        print('A')
-        if x > 0:
-            print('B')
-        print('C') 
-    def src2():
-        print('D') 
-`, /* We get below print('B'): */ ["End", ...rep(8, "ArrowUp"), ...rep(3, "Control+a")], "cut", ["Home", "ArrowDown", "ArrowDown"],`#(=> Strype:1:std
+        await testBeforeAfterPaste(page, classTestInput, /* We get below print('B'): */ ["End", ...rep(8, "ArrowUp"), ...rep(3, "Control+a")], "cut", ["Home", "ArrowDown", "ArrowDown"],`#(=> Strype:1:std
 #(=> Section:Imports
 #(=> Section:Definitions
 class Dest  :
@@ -258,20 +233,7 @@ class Src  :
 
     test("Test Ctrl-A four times in a class function selects whole class", async ({page}) => {
         // Moves Src class above dest: 
-        await testBeforeAfterPaste(page,`class Dest:
-   dest_member = 0
-   def dest(self):
-       print('0')
-class Src: 
-    src_member_1 = 0 
-    def src(self):
-        print('A')
-        if x > 0:
-            print('B')
-        print('C')
-    def src2():
-        print('D')
-`, /* We get below print('B'): */ ["End", ...rep(8, "ArrowUp"), ...rep(4, "Control+a")], "cut", ["Home"],`#(=> Strype:1:std
+        await testBeforeAfterPaste(page, classTestInput, /* We get below print('B'): */ ["End", ...rep(8, "ArrowUp"), ...rep(4, "Control+a")], "cut", ["Home"],`#(=> Strype:1:std
 #(=> Section:Imports
 #(=> Section:Definitions
 class Src  :
@@ -294,20 +256,7 @@ class Dest  :
 
     test("Test Ctrl-A five times in a class function selects all definitions", async ({page}) => {
         // Basically does nothing because everything ends where it began: 
-        await testBeforeAfterPaste(page,`class Dest:
-   dest_member = 0
-   def dest(self):
-       print('0')
-class Src: 
-    src_member_1 = 0
-    def src(self):
-        print('A')
-        if x > 0:
-            print('B')
-        print('C')
-    def src2():
-        print('D')
-`, /* We get below print('B'): */ ["End", ...rep(8, "ArrowUp"), ...rep(5, "Control+a")], "cut", ["Home"],`#(=> Strype:1:std
+        await testBeforeAfterPaste(page, classTestInput, /* We get below print('B'): */ ["End", ...rep(8, "ArrowUp"), ...rep(5, "Control+a")], "cut", ["Home"],`#(=> Strype:1:std
 #(=> Section:Imports
 #(=> Section:Definitions
 class Dest  :
@@ -323,6 +272,42 @@ class Src  :
         print('C') 
     def src2 (self, ) :
         print('D') 
+#(=> Section:Main
+#(=> Section:End
+`);
+    });
+    
+    const classTestInput2 = `class OneFunc:
+    def theOneFunc():
+        return 42
+class OneVar:
+    theOneVar = 43
+`;
+
+    test("Test Ctrl-A twice below solitary function in class selects whole class", async ({page}) => {
+        // Should move whole class: 
+        await testBeforeAfterPaste(page, classTestInput2, /* We get into the class: */ ["End", ...rep(5, "ArrowUp"), ...rep(2, "Control+a")], "cut", ["End"],`#(=> Strype:1:std
+#(=> Section:Imports
+#(=> Section:Definitions
+class OneVar  :
+    theOneVar  = 43 
+class OneFunc  :
+    def theOneFunc (self, ) :
+        return 42 
+#(=> Section:Main
+#(=> Section:End
+`);
+    });
+    test("Test Ctrl-A twice below solitary var in class selects whole class", async ({page}) => {
+        // Should move whole class: 
+        await testBeforeAfterPaste(page, classTestInput2, /* We get into the class: */ ["End", ...rep(2, "ArrowUp"), ...rep(2, "Control+a")], "cut", ["Home"],`#(=> Strype:1:std
+#(=> Section:Imports
+#(=> Section:Definitions
+class OneVar  :
+    theOneVar  = 43 
+class OneFunc  :
+    def theOneFunc (self, ) :
+        return 42 
 #(=> Section:Main
 #(=> Section:End
 `);
