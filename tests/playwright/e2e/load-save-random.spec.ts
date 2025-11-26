7936364f95677fa5f6ae6f47ea8796958bfe0166
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2025-11-26 21:57:59 +0000

    Fixed linter complaints.

--- tests/playwright/e2e/load-save-random.spec.ts
+++ tests/playwright/e2e/load-save-random.spec.ts
@@ -1093,13 +1093,13 @@ test.describe("Enters, saves and loads specific frames", () => {
 
     test("Enabled comments after disabled class and function", async ({page}) => {
         await testSpecific(page, [[], [
-            {frameType: "classdef", slotContent: ["Foo", "Class doc, this is before \' this is after\nThis is another line with \' in it\nThis last one too\'"], body: [], disabled: true},
+            {frameType: "classdef", slotContent: ["Foo", "Class doc, this is before ' this is after\nThis is another line with ' in it\nThis last one too'"], body: [], disabled: true},
             {frameType: "funcdef", slotContent: ["foo", "", "Func doc, this is before \" this is after\nThis is another line with \" in it\nThis last one too\""], body: [], disabled: true},
-            {frameType: "comment", slotContent: ["This has a single quote \' and a double quote\" in it."]},
-            {frameType: "comment", slotContent: ["This has paired single quotes \' like this \' and paired double quotes \" like this \" in it."]},
-            {frameType: "comment", slotContent: ["This has a single quote \' and a double quote\" in it.\nAnd a second line without."]},
-            {frameType: "comment", slotContent: ["This has paired single quotes \' like this \' and paired double quotes \" like this \" in it.\n And a second line too."]},
-        ], []], "Project doc, this is before \' this is after\nThis is another line with \" in it\nThis last one too\' \"\'");
+            {frameType: "comment", slotContent: ["This has a single quote ' and a double quote\" in it."]},
+            {frameType: "comment", slotContent: ["This has paired single quotes ' like this ' and paired double quotes \" like this \" in it."]},
+            {frameType: "comment", slotContent: ["This has a single quote ' and a double quote\" in it.\nAnd a second line without."]},
+            {frameType: "comment", slotContent: ["This has paired single quotes ' like this ' and paired double quotes \" like this \" in it.\n And a second line too."]},
+        ], []], "Project doc, this is before ' this is after\nThis is another line with \" in it\nThis last one too' \"'");
     });
     
     test("Empty classes", async ({page}) => {
@@ -1131,13 +1131,13 @@ test.describe("Enters, saves and loads specific frames", () => {
 
     test("Quote characters in descriptions and comments", async ({page}) => {
         await testSpecific(page, [[], [
-            {frameType: "classdef", slotContent: ["Foo", "Class doc, this is before \' this is after\nThis is another line with \' in it\nThis last one too\'"], body: []},
+            {frameType: "classdef", slotContent: ["Foo", "Class doc, this is before ' this is after\nThis is another line with ' in it\nThis last one too'"], body: []},
             {frameType: "funcdef", slotContent: ["foo", "", "Func doc, this is before \" this is after\nThis is another line with \" in it\nThis last one too\""], body: []},
-            {frameType: "comment", slotContent: ["This has a single quote \' and a double quote\" in it."]},
-            {frameType: "comment", slotContent: ["This has paired single quotes \' like this \' and paired double quotes \" like this \" in it."]},
-            {frameType: "comment", slotContent: ["This has a single quote \' and a double quote\" in it.\nAnd a second line without."]},
-            {frameType: "comment", slotContent: ["This has paired single quotes \' like this \' and paired double quotes \" like this \" in it.\n And a second line too."]},
-        ], []], "Project doc, this is before \' this is after\nThis is another line with \" in it\nThis last one too\' \"\'");
+            {frameType: "comment", slotContent: ["This has a single quote ' and a double quote\" in it."]},
+            {frameType: "comment", slotContent: ["This has paired single quotes ' like this ' and paired double quotes \" like this \" in it."]},
+            {frameType: "comment", slotContent: ["This has a single quote ' and a double quote\" in it.\nAnd a second line without."]},
+            {frameType: "comment", slotContent: ["This has paired single quotes ' like this ' and paired double quotes \" like this \" in it.\n And a second line too."]},
+        ], []], "Project doc, this is before ' this is after\nThis is another line with \" in it\nThis last one too' \"'");
     });
 
     test("Format strings", async ({page}) => {
