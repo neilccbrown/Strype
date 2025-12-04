ccdb6675b157c9ecc60826122603bcb02da497df
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2025-12-04 14:06:33 +0000

    Fixed the media tests, which need to be skipped on Chromium due to clipboard permission issues.

--- tests/playwright/e2e/structured-expressions-media.spec.ts
+++ tests/playwright/e2e/structured-expressions-media.spec.ts
@@ -7,10 +7,21 @@ test.beforeEach(async ({ page, browserName }, testInfo) => {
     if (process.platform === "win32" && browserName === "webkit") {
         testInfo.skip(true, "Skipping on WebKit + Windows due to clipboard permission issues.");
     }
-    await addFakeClipboard(page);
+    // These tests can take longer than the default 30 seconds:
+    testInfo.setTimeout(60000); // 60 seconds
+
+    if (browserName === "chromium") {
+        // Chromium prevents writing non-text to clipboard during headless mode so we can't test image copying:
+        testInfo.skip(true, "Skipping on Chromium due to clipboard permissions");
+    }
+    addFakeClipboard(page);
+    
     await page.goto("./", {waitUntil: "domcontentloaded"});
     await page.waitForSelector("body");
     //strypeElIds = await page.evaluate(() => (window as any)["StrypeHTMLELementsIDsGlobals"]);
+    await page.evaluate(() => {
+        (window as any).Playwright = true;
+    });
     // Make browser's console.log output visible in our logs (useful for debugging):
     page.on("console", (msg) => {
         console.log("Browser log:", msg.text());
@@ -19,6 +30,7 @@ test.beforeEach(async ({ page, browserName }, testInfo) => {
 
 test.describe("Media literal copying", () => {
     test("Test copying text with a media literal", async ({page}) => {
+        await page.keyboard.press("End");
         await page.keyboard.press("Backspace");
         await page.keyboard.press("Backspace");
         await page.keyboard.type("i");
@@ -49,6 +61,7 @@ test.describe("Media literal copying", () => {
         expect(clipboardItemCount).toEqual(1);
     });
     test("Test copying only image literal puts an image on clipboard", async ({page}, testInfo) => {
+        await page.keyboard.press("End");
         await page.keyboard.press("Backspace");
         await page.keyboard.press("Backspace");
         await page.keyboard.type("i");
@@ -58,53 +71,65 @@ test.describe("Media literal copying", () => {
         const image = fs.readFileSync("public/graphics_images/cat-test.jpg").toString("base64");
         await doPagePaste(page, image, "image/jpeg");
         await typeIndividually(page, ")");
-        let startIndex = "set_background(".length;
+        const startIndex = "set_background(".length;
         const endIndex = startIndex + 1;
         await doTextHomeEndKeyPress(page, false, false); // equivalent to "Home", see method for details
         await page.waitForTimeout(1000);
+        // First copy a single character to effectively clear the clipboard:
+        await page.keyboard.press("Shift+ArrowRight");
+        await page.waitForTimeout(200);
+        await page.keyboard.press("ControlOrMeta+c");
+        await page.waitForTimeout(300);
+        expect(await page.evaluate("navigator.clipboard.readText()")).toEqual("s");
+        // Back to start again:
+        await page.keyboard.press("ArrowLeft");
+        
         for (let i = 0; i < startIndex; i++) {
             await page.keyboard.press("ArrowRight");
             await page.waitForTimeout(1000);
         }
-        while (startIndex < endIndex) {
+        for (let i = startIndex; i < endIndex; i++) {
             await page.keyboard.press("Shift+ArrowRight");
             await page.waitForTimeout(75);
-            startIndex += 1;
         }
         await page.waitForTimeout(200);
         await page.keyboard.press("ControlOrMeta+c");
         await page.waitForTimeout(300);
-        const clipboardContent : string = await page.evaluate("navigator.clipboard.readText()");
-        expect(clipboardContent).toEqual("load_image(\"data:image/jpeg;base64," + image + "\")");
         const clipboardItemCount : string = await page.evaluate("navigator.clipboard.read().then((items) => items.length)");
-        expect(clipboardItemCount).toEqual(2);
+        expect(clipboardItemCount).toEqual(1);
         const clipboardImage : string = await page.evaluate(`
             navigator.clipboard.read().then(async (items) => {
-                for (const item of items) {
-                      for (const type of item.types) {
-                            if (type.startsWith("image/")) {
-                              const blob = await item.getType(type);
-                    
-                              // Convert Blob to base64
-                              const base64 = await new Promise((resolve, reject) => {
-                                const reader = new FileReader();
-                                reader.onloadend = () => resolve(reader.result);
-                                reader.onerror = reject;
-                                reader.readAsDataURL(blob); // Data URL includes base64-encoded image
-                              });
-                    
-                              return base64;
-                            }
+                const item = items[0];
+                for (const type of item.types) {
+                    if (type.startsWith("image/")) {
+                        const blob = await item.getType(type);
+                
+                        // Convert Blob to base64
+                        const base64 = await new Promise((resolve, reject) => {
+                            const reader = new FileReader();
+                            reader.onloadend = () => resolve(reader.result);
+                            reader.onerror = reject;
+                            reader.readAsDataURL(blob); // Data URL includes base64-encoded image
+                        });
+                
+                        return base64;
                       }
                 }
             });
 `);
         expect(clipboardImage).toEqual("data:image/jpeg;base64," + image);
+        
+        // Finally, try to paste it a second time in a frame inside:
+        await page.keyboard.press("ArrowDown");
+        await page.keyboard.type("i");
+        await page.waitForTimeout(100);
+        await typeIndividually(page, "set_background(");
     });
 });
 
 test.describe("Media literal manipulation", () => {
     test("Test surrounding an image literal with brackets", async ({page}) => {
+        await page.keyboard.press("End");
         await page.keyboard.press("Backspace");
         await page.keyboard.press("Backspace");
         await page.keyboard.type("i");
@@ -124,6 +149,7 @@ test.describe("Media literal manipulation", () => {
         await expect(page.locator("img[data-code^='load_image']")).toBeVisible();
     });
     test("Test surrounding a sound literal with brackets", async ({page}) => {
+        await page.keyboard.press("End");
         await page.keyboard.press("Backspace");
         await page.keyboard.press("Backspace");
         await page.keyboard.type("i");
