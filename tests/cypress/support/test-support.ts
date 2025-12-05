3067922deb9e5ebedaa3376ea007081d810bd800
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2025-12-05 22:42:48 +0000

    Adjusted test for use of &nbsp;

--- tests/cypress/support/test-support.ts
+++ tests/cypress/support/test-support.ts
@@ -1,6 +1,6 @@
 export function cleanFromHTML(html: string) : string {
     // Get rid of the zero-width spaces which occur in the HTML:
-    return html.replace("\u200B", "");
+    return html.replace("\u200B", "").replaceAll("\u00A0", " ");
 }
 
 export function getDefaultStrypeProjectDocumentationFullLine(mode: string): string {
