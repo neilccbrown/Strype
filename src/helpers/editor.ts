9e67c73081c27b93535e58f318ea38efd612194b
Pierre Weill-Tessier <pierre.weill-tessier@kcl.ac.uk>
2026-04-28 10:41:17 +0100

    Fixes an issue with copying expressions with textual operators from the editor (fixes #806)

--- src/helpers/editor.ts
+++ src/helpers/editor.ts
@@ -2125,6 +2125,13 @@ export function getEditableSelectionText() : string {
                 if (selectableText == "yes_quote") {
                     nodeContent = nodeContent.replaceAll(/[“”]/g, "\"").replaceAll(/[‘’]/g, "'");
                 }
+                else{
+                    // For the specific case when the node is an operator, we make sure we add spacing around the operator 
+                    // so that textual operators stay separated from their operands.
+                    if(node.parentElement?.classList.contains(scssVars.frameOperatorSlotClassName)){
+                        nodeContent = " " + nodeContent + " ";
+                    }
+                }
                 allNodes.push(nodeContent);
             }
         }
