c404ef222af9dc42eaaec2afacef97cef0fd4d3e
Pierre Weill-Tessier <pierre.weill-tessier@kcl.ac.uk>
2026-05-14 14:26:45 +0100

    Fix a typing error detected by the linter test

--- tests/playwright/support/general.ts
+++ tests/playwright/support/general.ts
@@ -1,8 +1,8 @@
-import { Page } from "@playwright/test";
+import { Disposable, Page } from "@playwright/test";
 
 // This stops Pyodide loading on the page.  Must be called before page.goto.
 // Useful for tests which don't actually execute the code at all.
-export function skipPyodideLoading(page: Page) : Promise<void> {
+export function skipPyodideLoading(page: Page) : Promise<Disposable> {
     return page.addInitScript(() => {
         (window as any).TestingNoPyodide = true;
     });
