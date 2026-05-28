01cb8099a1d8e454d8d2c9fade3edc09718c2abe
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2026-05-28 16:27:15 +0100

    Fixed the translation test with a proper wait after the page reload.

--- tests/cypress/e2e/translation.cy.ts
+++ tests/cypress/e2e/translation.cy.ts
@@ -163,6 +163,10 @@ describe("Locale persistence", () => {
             // Accept the change for a new project
             cy.wait(500);
             return cy.contains("button:visible", getLocalisedString("buttonLabel.continue", localeForTest)).click({force: true}).then(() => {
+                // Wait for the starting project to load fully:
+                cy.get(".frame-div", { timeout: 10000 })
+                    .should("have.length.at.least", 2);
+                
                 // Check the editor contains the initial state code: we download the conversion and compare with the backed up Python file
                 changeCodeThenDownloadPy({locale: localeForTest});
                 cy.wait(500);            
