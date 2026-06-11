70c2ba544bfa04f1a98bd9bd6c74f7d8ca808c6f
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2026-06-11 13:34:09 +0100

    Changed the asset files from being mounted at /strype/books to just /books, etc.

--- src/stryperuntime/pyodide-emscripten-assets-fs.ts
+++ src/stryperuntime/pyodide-emscripten-assets-fs.ts
@@ -2,7 +2,7 @@ import {createLazyFetchFS} from "@/stryperuntime/pyodide-emscript-fetch-fs";
 import {PyodideAPI} from "pyodide";
 import {EmscriptenFileSystemPlugin} from "@/types/emscripten-fs-types";
 
-const fileIndex: Record<string, string> = import.meta.glob(
+const assetsFileIndex: Record<string, string> = import.meta.glob(
     "/src/assetsFilesystem/**/*",
     {
         eager: true,
@@ -11,10 +11,15 @@ const fileIndex: Record<string, string> = import.meta.glob(
     }
 );
 
+export const assetsFilePrefixes = Array.from(new Set(Object.keys(assetsFileIndex).map((path) => {
+    const parts = path.replace(/^\/src\/assetsFilesystem\//, "").replace(/^\/+/, "").split("/");
+    return parts[0];
+})));
+
 // The content shouldn't change after page load so we have the cache as top-level item
 // so that we don't refetch the files on each run:
 const cache : Map<string, Uint8ClampedArray> = new Map();
 
 export function createLazyFetchAssetsFS(pyodide : PyodideAPI) : EmscriptenFileSystemPlugin {
-    return createLazyFetchFS(pyodide, fileIndex, undefined, cache);
+    return createLazyFetchFS(pyodide, assetsFileIndex, undefined, cache);
 }
